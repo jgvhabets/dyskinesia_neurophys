2de8dbbf190c836f954b7bef0e5e9e51be77ba8a
Jeroen Habets <jgvhabets@gmail.com>
2022-11-17 10:59:49 +0100

    ENH: added and tested SSD function based on mne.Decoding.SSD function

--- code/lfpecog_features/feats_spectral_helpers.py
+++ code/lfpecog_features/feats_spectral_helpers.py
@@ -125,3 +125,62 @@ def relative_power(psd):
 
 
     return relPsd
+
+
+from mne import create_info
+from mne.decoding import SSD
+
+def SSD_on_array(
+    array, fs, freqband_to_optim
+):
+    """
+    Spatial Signal Decompositino increases
+    the signal to noise ratio within a specific
+    freq-bandwidth by correcting the signal
+    for the 'noise-floor' measured in
+    neighbouring frequency bins
+
+    Based on mne.Decoding.SDD:
+    (https://mne.tools/stable/generated/mne.decoding.SSD.html#mne.decoding.SSD)
+    
+    Input:
+        - array to transform: should be without nans
+        - fs
+        - freqband_to_optim: tuple or list
+            with lower and higher freq border
+    
+    Returns:
+        - ssd_array: array with optimised SNR
+            for defined freq-band
+    
+    Raises:
+        - Assertion if array contains NaNs
+    """
+    assert ~np.isnan(array).any(), (
+        'array to perform SSD on cannot contain NaNs'
+    )
+    if len(array) == 1: array = np.array([array])  # transform to 2d array 1 x samples
+    if array.shape[0] > array.shape[1]: array = array.T  # transpose in horizontal format
+    
+    # create mne info object
+    ch_names=[f'n{n}' for n in range(array.shape[0])]
+    mne_info = create_info(
+        ch_names=ch_names,
+        sfreq=fs,
+        ch_types=['eeg'] * len(ch_names)
+    )
+    
+    freqs_noise = (freqband_to_optim[0] - 1, freqband_to_optim[1] + 1)
+
+    ssd = SSD(info=mne_info,
+        reg='oas',
+        sort_by_spectral_ratio=False,  # False for purpose of example.
+        filt_params_signal=dict(l_freq=freqband_to_optim[0],h_freq=freqband_to_optim[1],
+                                l_trans_bandwidth=1, h_trans_bandwidth=1),
+        filt_params_noise=dict(l_freq=freqs_noise[0], h_freq=freqs_noise[1],
+                               l_trans_bandwidth=1, h_trans_bandwidth=1),
+    )
+    # perform SSD
+    ssd_array = ssd.fit_transform(array)
+
+    return ssd_array
