7abc2d9fdd6e00940b2657fab56195ebab3377d1
Jeroen Habets <jgvhabets@gmail.com>
2022-11-30 18:43:00 +0100

    ENH: added file to store MNEObjects within a class as a pickled file. This improves loading speed by factor 100

---
+++ code/utils/utils_pickle_mne.py
@@ -0,0 +1,28 @@
+"""
+Function to store MNE obects as pickle
+"""
+
+# import puiblic packages
+from os.path import join
+import numpy as np
+from dataclasses import dataclass, field
+from typing import Any
+
+# import own functions
+from utils.utils_fileManagement import get_project_path
+
+
+
+@dataclass(init=True, repr=True)
+class pickle_EpochedArrays:
+
+    mne_object: Any = None
+    list_mne_objects: list = field(default_factory=list)
+    
+    def __post_init__(self,):
+
+        if len(self.list_mne_objects) >= 1:
+            self.info = self.list_mne_objects[0].info
+        
+        else:
+            self.info = self.mne_object.info
