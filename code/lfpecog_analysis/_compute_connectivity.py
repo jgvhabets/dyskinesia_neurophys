5dcca03538d2739f9b15a4f47c4af3aa1c27201a
Thomas Samuel Binns <t.s.binns@outlook.com>
2023-10-31 14:17:08 +0100

    bug fix empty and small cons

--- code/lfpecog_analysis/_compute_connectivity.py
+++ code/lfpecog_analysis/_compute_connectivity.py
@@ -90,6 +90,8 @@ def compute_connectivity(
             rank=window_rank,
             min_n_chans=window_rank,
         )
+        if small_cons != []:
+            small_cons = [con_idx + len(empty_cons) for con_idx in small_cons]
         bad_cons = np.unique(empty_cons + small_cons).tolist()
 
         if len(bad_cons) < n_cons:
